b3f0f6d9404a4553b327e3910c1510ed52dfa47d
Mutuku1998 <martinmutuku1998@gmail.com>
2024-09-03 12:12:52 +0300

    icon correction

--- script.js
+++ script.js
@@ -5,31 +5,27 @@
     const iconEL = speakEL.querySelector('i');
 
     speakEL.addEventListener('click', toggleTTS);
-   
 
     function toggleTTS() {
         isTTSActive = !isTTSActive; 
 
         if (isTTSActive) {
-            iconEL.classList.remove('fa-microphone');
-            iconEL.classList.add('fa-microphone-slash');
-            document.addEventListener('mouseup', speakSelectedText);
-        } else {
             iconEL.classList.remove('fa-microphone-slash');
             iconEL.classList.add('fa-microphone');
+            document.addEventListener('mouseup', speakSelectedText);
+        } else {
+            iconEL.classList.remove('fa-microphone');
+            iconEL.classList.add('fa-microphone-slash');
             window.speechSynthesis.cancel(); 
             document.removeEventListener('mouseup', speakSelectedText);
         }
     }
 
     function speakSelectedText() {
-        
         window.speechSynthesis.cancel();
 
-        
         const selectedText = window.getSelection().toString();
 
-      
         if (selectedText) {
             const utterance = new SpeechSynthesisUtterance(selectedText);
             window.speechSynthesis.speak(utterance);
